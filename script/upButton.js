efab5e40e5fe47bc3da1fffa3b362969e93f56b0
bohdanahostynska <111461462+bohdanahostynska@users.noreply.github.com>
2023-06-06 13:40:22 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ script/upButton.js
@@ -0,0 +1,49 @@
+$(window).on('scroll',function(){
+    if($(this).scrollTop()>500){
+        $('#up').fadeIn()
+    }else{
+        $('#up').fadeOut()
+    }
+})
+
+$("#up").on("click",function(){
+    $("html,body").animate({scrollTop:0},500)
+})
+
+$('#about-link').on('click',function(e){
+    e.preventDefault();
+    $('html, body') .animate({scrollTop: $('#about').offset().top}, 4000)
+
+})
+
+$("#up").on("click",function(){
+    $("html,body").animate({scrollTop:0},500)
+})
+
+$('#pictures-link').on('click',function(e){
+    e.preventDefault();
+    $('html, body') .animate({scrollTop: $('#pictures').offset().top}, 4000)
+
+})
+
+$('#menuToggle #toggleCheckbox').change(function(){
+    if(this.checked){
+        $('body').css({'overflow':"hidden"})
+    }
+    else{
+        $('body').css({'overflow':"visible"})
+    }
+});
+
+$('#up').on('click',function(){
+    $('html,body').animate({scrollTop:0},500)
+})
+
+$('#audience_link').on('click',function(e){
+    e.preventDefault();
+    document.getElementById('toggleCheckbox').checked=false
+    $('body').css({'overflow':"visible"})
+    $('html, body').animate({scrollTop:$('#audience').offset().top})
+})
+
+
